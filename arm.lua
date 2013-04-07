a4b6d61ac0ea232711d369fde4e7143f14e40adb
Zju Toe <zjutoe@gmail.com>
2013-04-07 18:04:33 +0800

    bit operations refining

--- arm.lua
+++ arm.lua
@@ -1,20 +1,17 @@
-require 'luabit/bit'
+--require 'luabit/bit'
+require 'utils'
 
 -- get v[n:m]
 local function bits(v, n, m)
-   if n<m or n>31 or m<0 then
-      return nil
-   end
-   --return bit.blogic_rshift(bit.blshift(v, 31-n), 31-n+m)
-   return bit.brshift(bit.blshift(v, 31-n), 31-n+m)
+   return bit.bits(v, n, m)
 end
 
 local function rotate_left(v, n)
-   return bit.bor(bit.blshift(v, n), bit.brshift(v, 32-n))
+   return bit.rleft(v, n)
 end
 
 local function rotate_right(v, n)
-   return bit.bor(bit.brshift(v, n), bit.blshift(v, 32-n))
+   return bit.rright(v, n)
 end
 
 
@@ -39,80 +36,66 @@ end
 
 dump_regs(R)
 
-
 function cond(inst, cpsr)
 
-   local c = bit.brshift(inst, 28) -- inst[31:28]
+   local c = bits(inst, 31, 28)	-- inst[31:28]
    
+   -- to check the CPSR[31:28], i.e. {N, Z, C, V}
    if c == 0 then 
-      -- Equal
-      -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x40000000) ~= 0
+      -- Equal, Z set
+      return bits(cpsr, 30, 30) == 1
    elseif c == 1 then
       -- NE: Z clear
       -- Not Equal
-       -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x40000000) == 0
+      return bits(cpsr, 30, 30) == 0
    elseif c == 2 then
       -- CS/HS: C set
       -- Carry set/unsigned higher or same
-      -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x20000000) ~= 0
+      return bits(cpsr, 29, 29) == 1
    elseif c == 3 then
       -- CC/LO: C clear 
       -- Carry clear/unsigned lower
-       -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x20000000) == 0
+      return bits(cpsr, 29, 29) == 0
    elseif c == 4 then
       -- MI: N set
       -- Minus/negative
-       -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x80000000) ~= 0
+      return bits(cpsr, 31, 31) == 1
    elseif c == 5 then
       -- PL: N clear
       -- Plus/positive or zero
-       -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x80000000) == 0
+      return bits(cpsr, 31, 31) == 0
    elseif c == 6 then
       -- VS: V set
       -- Overflow
-       -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x10000000) ~= 0
+      return bits(cpsr, 28, 28) == 1
    elseif c == 7 then
       -- VC: V clear
       -- No overflow
-      -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x10000000) == 0
+      return bits(cpsr, 28, 28) == 0
    elseif c == 8 then
       -- HI: C set and Z clear
       -- Unsigned higher
-      -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x20000000) ~= 0 and bit.band(cpsr, 0x40000000) == 0
+      return bits(cpsr, 29, 29) == 1 and bits(cpsr, 30, 30) == 0
    elseif c == 9 then
       -- LS: C clear or Z set
       -- Unsigned lower or same
-      -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x20000000) == 0 and bit.band(cpsr, 0x40000000) ~= 0
+      return bits(cpsr, 29, 29) == 0 or bits(cpsr, 30, 30) == 1
    elseif c == 10 then
       -- GE: N set and V set, or N clear and V clear (N == V)
       -- Signed greater than or equal
-      -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x80000000) == bit.band(cpsr, 0x10000000)
+      return bits(cpsr, 31, 31) == bits(cpsr, 28, 28)
    elseif c == 11 then
       -- LT: N set and V clear, or N clear and V set (N != V)
       -- Signed less than
-      -- CPSR[31:28] = {N, Z, C, V}
-      return bit.band(cpsr, 0x80000000) ~= bit.band(cpsr, 0x10000000)
+      return bits(cpsr, 31, 31) ~= bits(cpsr, 28, 28)
    elseif c == 12 then
       -- GT: Z clear, and either N set and V set, or N clear and V clear (Z == 0,N == V)
       -- Signed greater than
-      -- CPSR[31:28] = {N, Z, C, V}
-      return  bit.band(cpsr, 0x40000000) == 0 and bit.band(cpsr, 0x80000000) == bit.band(cpsr, 0x10000000)
+      return  bits(cpsr, 30, 30) == 0 and bits(cpsr, 31, 31) == bits(cpsr, 28, 28)
    elseif c == 13 then
       -- LE: Z set, or N set and V clear, or N clear and V set (Z == 1 or N != V)
       -- Signed less than or equal
-      -- CPSR[31:28] = {N, Z, C, V}
-      return  bit.band(cpsr, 0x40000000) ~= 0 or bit.band(cpsr, 0x80000000) ~= bit.band(cpsr, 0x10000000)
+      return  bits(cpsr, 30, 30) == 1 or bits(cpsr, 31, 31) ~= bits(cpsr, 28, 28)
    elseif c == 14 then
       -- AL: 
       -- Always (unconditional)
@@ -133,6 +116,12 @@ function cond(inst, cpsr)
    
 end
 
+
+-- inst: table of bits of instruction
+-- cpsr: table of bits of CPSR
+function get_shifter_operand(inst, cpsr)
+   
+end
 
 function opcode(inst, cpsr)
    
@@ -164,7 +153,7 @@ function opcode(inst, cpsr)
 	 -- A5.1.5 Data-processing operands - Logical shift left by immediate
 	 local shift_imm = bits(inst, 11, 7)
 	 local Rm = bits(inst, 3, 0)
-	 shifter_operand = rotate_left(R[Rm], shift_imm) -- FIXME should be Logical_Shift_Left
+	 shifter_operand = bits.sll(R[Rm], shift_imm) -- FIXME should be Logical_Shift_Left
 	 if shift_imm == 0 then
 	    shifter_carry_out = cflag
 	 else
@@ -172,9 +161,39 @@ function opcode(inst, cpsr)
 	 end
       elseif bits(inst, 7, 4) == 1 then
 	 -- A5.1.6 Data-processing operands - Logical shift left by register
+	 local Rs = bits(inst, 11, 8)
+	 local Rm = bits(inst, 3, 0)
+	 local shift = bits(R[Rs], 7, 0)
 	 
+	 if shift == 0 then
+	    shifter_operand = R[Rm]
+	    shifter_carry_out = cflag
+	 elseif shift < 32 then
+	    shifter_operand = bits.sll(R[Rm], shift)
+	    shifter_carry_out = bits(R[Rm], 32-shift, 32-shift)
+	 elseif shift == 32 then
+	    shifter_operand = 0
+	    shifter_carry_out = bits(R[Rm], 0, 0)
+	 else
+	    shifter_operand = 0
+	    shifter_carry_out = 0
+	 end	 
+      elseif bits(inst, 6, 4) == 2 then
+	 -- A5.1.7 Data-processing operands - Logical shift right by immediate
+	 local shift_imm = bits(inst, 11, 7)
+	 local Rm = bits(inst, 3, 0)
+
+	 if shift_imm == 0 then
+	    shifter_operand = 0
+	    shifter_carry_out = bits(R[Rm], 31, 31)
+	 else
+	    shifter_operand = bits.slr(R[Rm], shift_imm)
+	    shifter_carry_out = bits(R[Rm], shift_imm-1, shift_imm-1)
+	 end
+	    
+
       end
-      
+
       if bits(inst, 4, 4) == 0 then
 	 local shift_imm = bits(inst, 11, 7)
 	 local shift = bits(inst, 6, 5)
---
+++ utils.lua
@@ -0,0 +1,248 @@
+local function tobits(v)
+   -- TODO assert(v>=0)
+   -- v <= 2^32-1
+   if v > 4294967295 then
+      return nil
+   end
+
+   local t = {}
+   local i = 0
+   
+   while v > 0 do
+      local remain = v % 16
+      v = (v - remain) / 16
+      
+      if remain == 0 then	 
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,0,0,0	
+      elseif remain == 1 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,0,0,1	
+      elseif remain == 2 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,0,1,0	
+      elseif remain == 3 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,0,1,1	
+      elseif remain == 4 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,1,0,0	
+      elseif remain == 5 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,1,0,1	
+      elseif remain == 6 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,1,1,0	
+      elseif remain == 7 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 0,1,1,1	
+      elseif remain == 8 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,0,0,0	
+      elseif remain == 9 then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,0,0,1	
+      elseif remain == 0xa then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,0,1,0	
+      elseif remain == 0xb then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,0,1,1	
+      elseif remain == 0xc then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,1,0,0	
+      elseif remain == 0xd then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,1,0,1	
+      elseif remain == 0xe then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,1,1,0	
+      elseif remain == 0xf then
+	 t[i+3],t[i+2],t[i+1],t[i] = 1,1,1,1	
+      end
+
+      i = i + 4
+   end
+
+   for i=i, 31 do
+      t[i] = 0
+   end
+
+   t.size = 32
+   
+   return t
+end
+
+local function sub_tonum(t, i, j)
+   if i<j or i>31 or j<0 then
+      return nil
+   end
+
+   local n = 0
+   for k=i, j, -1 do
+      n = n + n + t[k]
+   end
+
+   return n
+end
+
+local function bits_v(v, i, j)
+   return sub_tonum(tobits(v), i, j)
+end
+
+
+local function sub(t, m, n)
+   if m<n or m>31 or n<0 then return nil end
+
+   local t2 = {}
+   local i = 0
+   for k=n, m do
+      t2[i] = t[k]
+      i = i +1
+   end
+
+   t2.size = i
+
+   return t2
+end
+
+local function append(t1, t2)
+   if t1.size + t2.size > 32 then return nil end
+
+   local t3 = {}
+   local k = t1.size-1
+   for i=t1.size+t2.size-1, t1.size, -1 do
+      t3[i] = t1[k]
+      k = k -1
+   end
+   for i=t2.size-1, 0, -1 do
+      t3[i] = t2[i]
+   end   
+
+   t3.size = t1.size + t2.size
+
+   return t3
+end
+
+local function fill_logic(t)
+   local t2 = {}
+   
+   for i=0, t.size-1 do
+      t2[i] = t[i]
+   end
+   for i=t.size, 31 do
+      t2[i] = 0
+   end
+
+   t2.size = 32
+
+   return t2   
+end
+
+local function rotate_left(t, n)
+   local t1 = sub(t, 31-n, 0)
+   local t2 = sub(t, 31, 32-n)
+   return append(t1, t2)
+end
+
+local function rotate_right(t, n)
+   local t1 = sub(t, n-1, 0)
+   local t2 = sub(t, 31, n)
+   return append(t1, t2)
+end
+
+local function shift_logic_left(t, n)
+   local t1 = sub(t, 31-n, 0)
+   local t2 = {}
+   for i=0, n-1 do
+      t2[i] = 0
+   end
+   t2.size = n
+   return append(t1, t2)
+end
+
+local function shift_logic_right(t, n)
+   local t1 = sub(t, 31, n)
+   local t2 = {}
+   for i=0, n-1 do
+      t2[i] = 0
+   end
+   t2.size = n
+   return append(t2, t1)
+end
+
+
+local function rotate_left_v(v, n)
+   if n>32 or n<0 then return nil end
+
+   local b = tobits(v)
+   local t = {}
+
+   local j = 31
+   for i=31-n, 0, -1 do
+      t[j] = b[i]
+      j = j - 1
+   end
+   for i=31, 32-n, -1 do
+      t[j] = b[i]
+      j = j - 1
+   end
+
+   return tonumber(t)
+end
+
+local function rotate_right_v(v, n)
+   if n>32 or n<0 then return nil end
+
+   local b = tobits(v)
+   local t = {}
+
+   local j = 0
+   for i=n, 31 do
+      t[j] = b[i]
+      j = j + 1
+   end
+   for i=0, n-1 do
+      t[j] = b[i]
+      j = j + 1
+   end
+
+   return tonumber(t)
+end
+
+local function logical_left_shift_v(v, n)
+   if n>32 or n<0 then return nil end
+   
+   local b = tobits(v)
+   local t = {}
+
+   local j = 31
+   for i=31-n, 0, -1 do
+      t[j] = b[i]
+      j = j - 1
+   end
+   for i=31, 32-n, -1 do
+      t[j] = 0
+      j = j -1
+   end      
+
+   return tonumber(t)
+end
+
+local function logical_right_shift_v(v, n)
+   if n>32 or n<0 then return nil end
+
+   local b = tobits(v)
+   local t = {}
+
+   local j = 0
+   for i=n, 31 do
+      t[j] = b[i]
+      j = j + 1
+   end
+   for i=0, n-1 do
+      t[j] = 0
+      j = j + 1
+   end
+
+   return tonumber(t)
+end
+
+
+bit = {
+tobits = tobits,
+tonum = tonum,
+bits = bits_v,
+rleft = rotate_left_v,
+rright = rotate_right_v,
+sll = logical_left_shift_v,
+slr = logical_right_shift_v,
+sub = sub,
+append = append,
+fill_logic = fill_logic,
+}
